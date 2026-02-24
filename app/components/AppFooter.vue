b261695bfc7804f3407817dc7f16494b50fe4f72
Harris Rashid <holla@harrisrashid.com>
2026-02-23 21:04:16 -0500

    Footer styles

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -6,17 +6,17 @@ defineProps<{ settings?: Content.SettingsDocument }>()
 
 <template>
   <footer>
-    <div class="bg-amber-950 mt-24 pb-24 lg:pb-0 relative">
+    <div class="bg-stone-900 mt-24 pb-24 lg:pb-0 relative">
       <!-- <div class="absolute bottom-0 left-40 z-0 opacity-90">
         <Icon name="game-icons:anvil" class="bg-icon opacity-10"/>
       </div> -->
       <Bounded>
         <div class="lg:flex gap-x-16 z-10 relative pb-24">  
           <div class="lg:w-1/2 lg:mb-0 pb-24 ">
-            <h2 class="mb-6">Interested in 
-              <span class="block mt-4 text-4xl lg:text-5xl text-amber-700">Joining the Space?</span>
+            <h2 class="mb-6 text-stone-600">Interested in 
+              <span class="block mt-4 text-4xl lg:text-5xl text-white">Joining the Space?</span>
             </h2>
-            <div class="mt-2 p-4 lg:p-8 rounded-lg border border-1 border-amber-800 bg-amber-900/80">
+            <div class="mt-2 p-4 lg:p-8 rounded-lg border border-1 border-stone-800 bg-stone-950/80">
               <!-- <h3 class="text-white font-bold">Steve Campos</h3>
               <div>Owner</div>
               <div class="mb-2">Steel Fabricated Arts</div>
@@ -28,33 +28,36 @@ defineProps<{ settings?: Content.SettingsDocument }>()
           <div class="lg:w-1/2">
           <form class="space-y-12" name="contact" method="POST" data-netlify="true" netlify>        
             <div>
-              <label class="block text-sm font-medium text-amber-800">Your Name
+              <label class="block text-sm font-medium text-stone-400">Your Name
               <input type="text" name="name"
-                class="text-xl block w-full px-3 py-2 mt-1 text-stone-900 bg-amber-700 border border-amber-700 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-800 focus:border-gray-800"></label>
+                class="text-xl block w-full px-3 py-2 mt-1 text-stone-600 bg-stone-300 border border-stone-700 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-800 focus:border-gray-800"></label>
             </div>
             <div>
-              <label class="block text-sm font-medium text-amber-800">Email
+              <label class="block text-sm font-medium text-stone-400">Email
               <input type="email" name="email" 
-                class="text-xl block w-full px-3 py-2 mt-1 text-stone-900 bg-amber-700 border border-amber-700  rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-800 focus:border-gray-800"></label>
+                class="text-xl block w-full px-3 py-2 mt-1 text-stone-600 bg-stone-300 border border-stone-700  rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-800 focus:border-gray-800"></label>
             </div>
             <div>
-              <label class="block text-sm font-medium text-amber-800">Your Message
+              <label class="block text-sm font-medium text-stone-400">Your Message
               <textarea name="message"
-                class="text-xl block h-24 w-full px-3 py-2 mt-1 text-stone-900 bg-amber-700 border border-amber-700  rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-800 focus:border-gray-800">
+                class="text-xl block h-24 w-full px-3 py-2 mt-1 text-stone-600 bg-stone-300 border border-stone-700  rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-800 focus:border-gray-800">
                 </textarea></label>
             </div>
-            <button type="submit" 
-              class="text-2xl mx-auto mt-12 buttonLink">
+            <div class="text-center">
+              <button type="submit" 
+              class="text-2xl mx-auto buttonLink">
               Send Now
-            </button>
+              </button>
+            </div>
+              
           </form>
           </div>
         </div>
       </Bounded>         
     </div>
-    <div class="bg-amber-900">
+    <div class="bg-stone-950">
       <nav
-        class="flex flex-col items-center justify-between gap-6 px-8 py-7 md:flex-row border-t border-amber-950"
+        class="flex flex-col items-center justify-between gap-6 px-8 py-7 md:flex-row border-t border-stone-950"
         aria-label="Footer"
       >
       <NuxtLink to="/">
@@ -63,7 +66,7 @@ defineProps<{ settings?: Content.SettingsDocument }>()
       </NuxtLink>
       <ul class="lg:flex gap-6 text-center lg:text-left">
         <li v-for="link in settings?.data.navigation" :key="link.key">
-          <PrismicLink :field="link" class="inline-flex min-h-11 items-center text-black hover:text-amber-500" />
+          <PrismicLink :field="link" class="inline-flex min-h-11 items-center text-stone-400 hover:text-stone-500" />
         </li>
       </ul>
       </nav>
